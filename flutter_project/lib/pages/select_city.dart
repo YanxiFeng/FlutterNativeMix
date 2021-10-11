09a30d5979617a6b61f3dda991787cca70eceb69
Yvan <ios.sysail@gmail.com>
2021-10-11 21:07:44 +0800

    ListView

--- flutter_project/lib/pages/select_city.dart
+++ flutter_project/lib/pages/select_city.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SelectCityPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: BackButton(
+          onPressed: () => {Navigator.pop(context, false)},
+        ),
         title: Text("Select City"),
       ),
-      body: ListView.builder(
-          itemCount: 8,
+      body: ListView.separated(
+          padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+          shrinkWrap: true,
+          physics: AlwaysScrollableScrollPhysics(),
+          cacheExtent: 40,
           itemBuilder: (context, index) {
-            return Text("data");
-          }),
+            return ListTile(
+              title: Text(
+                "Singapore1",
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600),
+              ),
+              trailing: Icon(Icons.check),
+              contentPadding: EdgeInsets.fromLTRB(46, 0, 24, 0),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider(
+              color: Colors.grey,
+              indent: 46,
+            );
+          },
+          itemCount: 8),
     );
   }
 }
